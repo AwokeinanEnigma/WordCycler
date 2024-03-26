d7aa05560f9a782a3da6f3a61e5f106e39bd4570
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2024-03-26 09:42:56 -0400

    minor refactorings

--- HelloWorldCycler/Program.cs
+++ HelloWorldCycler/Program.cs
@@ -7,131 +7,135 @@ namespace HelloWorldCycler
     class Program
     {
         // initialize alphabet list
-        public static List<string> characters = new List<string> {
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-            "g",
-            "h",
-            "i",
-            "j",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-            "p",
-            "q",
-            "r",
-            "s",
-            "t",
-            "u",
-            "v",
-            "w",
-            "x",
-            "y",
-            "z",
+        static readonly List<char> _Characters = new List<char> {
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z',
             // empty space
-            " ",
+            ' ',
             // CAPITALS 
-            "A",
-            "B",
-            "C",
-            "D",
-            "E",
-            "F",
-            "G",
-            "H",
-            "I",
-            "J",
-            "K",
-            "L",
-            "M",
-            "N",
-            "O",
-            "P",
-            "Q",
-            "R",
-            "S",
-            "T",
-            "U",
-            "V",
-            "W",
-            "X",
-            "Y",
-            "Z",
+            'A',
+            'B',
+            'C',
+            'D',
+            'E',
+            'F',
+            'G',
+            'H',
+            'I',
+            'J',
+            'K',
+            'L',
+            'M',
+            'N',
+            'O',
+            'P',
+            'Q',
+            'R',
+            'S',
+            'T',
+            'U',
+            'V',
+            'W',
+            'X',
+            'Y',
+            'Z',
             // numbers
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "0",
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '0',
             // misc
-            "/",
-            "\\", // this is represented as a single backslash i think?
-            "-",
-            "+",
-            "~",
-            "'",
-            ",",
-            "!",
-            "?",
-            "’", // weird apostrophe..?
-            ".",
-            ".",
-            "“",
-            "”",
+            '/',
+            '\\', // this is represented as a single backslash i think?
+            '-',
+            '+',
+            '~',
+            '\'',
+            ',',
+            '!',
+            '?',
+            '’', // weird apostrophe..?
+            '.',
+            '.',
+            '“',
+            '”',
         };
 
         static void Main(string[] args)
         {
-            string filler = string.Empty;
-            // enter funny string
             Console.WriteLine("Enter some words!");
+
+            // read input
             string helloWorld = Console.ReadLine();
-            char[] helloWorldLetters = helloWorld.ToCharArray();
-            helloWorldLetters.ToString();
-            int counter = 0;
-
-        restart:
-            foreach (string letter in characters)
+            while (string.IsNullOrEmpty(helloWorld))
             {
-
+                Console.WriteLine("Please enter some words!");
+                helloWorld = Console.ReadLine();
+            }
+            char[] helloWorldLetters = helloWorld.ToCharArray();
+            int counter = 0;
+            
+            string filler = string.Empty;
+            
+            // this label does all of the work
+            main:
+            
+            // loop through our characters
+            foreach (char letter in _Characters)
+            {
+                // this creates a cool effect
                 Console.WriteLine(filler + letter);
-                //Console.WriteLine(helloWorldLetters[counter].ToString());
-
-                // toString is needed here
-                if (letter == helloWorldLetters[counter].ToString())
+                if (letter == helloWorld[counter])
                 {
-                    filler = filler + letter;
-                    //Console.WriteLine(filler);
+                    filler += letter;
                     counter++;
-
-
+                    
+                    // if we're done
                     if (counter >= helloWorldLetters.Length)
                     {
                         goto end;
                     }
-                    else
-                    {
-                        // purely for show
-                        Thread.Sleep(62);
-                        // restart loop
-                        goto restart;
-                    }
+                    // purely for show
+                    Thread.Sleep(62);
+                    
+                    // if we've found the letter, we need to break out of the loop and restart it
+                    goto main;
                 }
             }
-        end:
-            Console.ReadLine();
-            //Console.WriteLine(filler);
+            
+            end:
+            // don't close the console until the user presses a key 
+            Console.ReadKey();
         }
     }
 }
